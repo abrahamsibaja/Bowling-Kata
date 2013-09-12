ed307ca2fb8cfa8bd74df5c756473789b6a1b789
Abraham <asibaja@pernix-solutions.com>
2013-09-12 11:48:17 -0600

    REFACTOR: missResponsability game_spec

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -2,14 +2,18 @@ module Bowling
   class Game
 
     def initialize
-      @score = 0
+      @rolls = Array.new(21)
+      @currentRoll = 0
     end
 
     def roll(pins)
-      @score += pins
+      @rolls[@currentRoll] = pins
+      @currentRoll += 1
     end
 
     def score
+      @score = 0
+      @rolls.map {|i| @score += i.to_i}
       return @score
     end
   end
