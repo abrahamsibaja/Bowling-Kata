9985b1dd9c5b0b7f03cf47cfdb456864625e01ff
Abraham <asibaja@pernix-solutions.com>
2013-09-12 10:33:52 -0600

    REFACTOR: game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,15 +3,11 @@ require 'spec_helper'
 
 module Bowling
   describe Game do
-    describe "canCreateGame" do
-      it "createsNewGame" do
-        game = Game.new
-      end
-      describe "canRoll" do
-        it "rolls" do
-          game = Game.new
-          game.roll(0)
-        end
+    let(:game) {Game.new}
+
+    describe "canRoll" do
+      it "rolls" do
+        game.roll(0)
       end
     end
   end
